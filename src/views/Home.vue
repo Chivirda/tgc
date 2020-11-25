49ccb889c670dd13194e6a3be3bc039e071feb7e
Sergey Chivirda <si.chivirda@gmail.com>
2020-11-25 21:08:47 +0300

    feat: remove /public

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="login-wrap">
+      <form class="login-form">
+        <span class="login-form-title">
+          Sign In
+        </span>
+
+        <div class="input-wrap">
+          <input type="text" class="form-input" name="name" placeholder="Username">
+        </div>
+
+        <div class="input-wrap">
+          <input type="password" name="password" id="password" class="form-input" placeholder="Password">
+        </div>
+
+        <div class="button-wrap">
+          <button @click.prevent="onSubmitHandler" class="login-form-button">
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  methods: {
+    onSubmitHandler() {
+      console.log('Form submitted!')
+    }
   }
 }
 </script>
