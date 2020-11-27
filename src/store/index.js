7f36687416043d0f02be48e19ae2f2b25e39d7da
Sergey Chivirda <si.chivirda@gmail.com>
2020-11-28 00:36:31 +0300

    feat: finish autorization and get token

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import qs from 'qs'
 
 Vue.use(Vuex)
 
@@ -31,13 +32,13 @@ export default new Vuex.Store({
     login({commit}, user) {
       return new Promise((resolve, reject) => {
         commit('auth_request')
-        axios.post('http://aspt.tgc2-energo.ru/token', user)
+        axios.post('http://aspt.tgc2-energo.ru/token', qs.stringify(user))
           .then(resp => {
-            const token = resp.data.token
+            const token = resp.data.access_token
             const user = resp.data.user
             localStorage.setItem('token', token)
             commit('auth_success', token, user)
-            console.info(token)
+            console.info('Auth success! Token:', token)
             resolve(resp)
           })
           .catch(err => {
