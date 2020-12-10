184238bff46b8345f1a1b5ddf74a1836709b7399
Sergey Chivirda <si.chivirda@gmail.com>
2020-12-10 21:36:05 +0300

    fix: refactor

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import isLoggedIn from '@/assets/isLoggedIn'
 
 
 Vue.use(VueRouter)
@@ -37,24 +38,10 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token')
-
-  function parseJwt (token) {
-    var base64Url = token.split('.')[1]
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-    }).join(''))
-
-    return JSON.parse(jsonPayload)
-  }
-
-  const expireTime = Date(parseJwt(token).exp)
-  const now = Date.now() / 1000
-  console.log('Now', Date(now))
+  
   const requireAuth = to.matched.some(record => record.meta.auth)
 
-  if (requireAuth && expireTime < now) {
+  if (requireAuth && !isLoggedIn) {
     next('/login')
   } else {
     next()
